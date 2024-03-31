e40233d43eeb50bde6bdf31262ccf5be4fbd7238
diego <dfmontesinos@gmail.com>
2024-03-31 10:17:51 +0200

    finish section 3 pictures, starting the next sections

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Home.css";
+import { Button } from "semantic-ui-react";
 
 function Home() {
   return (
@@ -11,10 +12,37 @@ function Home() {
         />
       </section>
       <section className="three-articles-container">
-        <article>Article 1</article>
-        <article>Article 2</article>
-        <article>Article 3</article>
+        <article>
+          <img
+            src="https://picsum.photos/id/7/250/270"
+            alt="opn book in a table"
+          />
+          <h3>Online shop, pick up at store</h3>
+          <Button className="buttonsHome">See more</Button>
+        </article>
+        <article>
+          <img
+            src="https://picsum.photos/id/20/250/270"
+            alt="opn book in a table"
+          />
+          <h3>Author's signed books</h3>
+          <Button className="buttonsHome">See more</Button>
+        </article>
+        <article>
+          <img
+            src="https://picsum.photos/id/635/250/270"
+            alt="opn book in a table"
+          />
+          <h3>Join our club and have special discounts</h3>
+          <Button className="buttonsHome purple">
+            I want to be in the club
+          </Button>
+        </article>
       </section>
+      <div>
+        <section></section>
+        <section></section>
+      </div>
     </div>
   );
 }
