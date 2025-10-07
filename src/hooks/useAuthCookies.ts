4688b9b9b163fb0f4e3e7db7b20c59b2adf6993b
Diego <dfmontesinos@gmail.com>
2025-10-07 15:32:57 +0200

    Added encripting to the localstorage

--- src/hooks/useAuthCookies.ts
+++ src/hooks/useAuthCookies.ts
@@ -1,15 +1,25 @@
 import { useCallback } from "react";
+import { decrypt, encrypt } from "../utils/encrypter/Encrypter";
 
 const STORAGE_KEY = "isAuthorized";
 
 export function useAuthStorage() {
   const setAuth = useCallback((value: boolean) => {
-    localStorage.setItem(STORAGE_KEY, JSON.stringify(value));
+    const encryptedValue = encrypt(value);
+    if (encryptedValue !== undefined) {
+      localStorage.setItem(STORAGE_KEY, encryptedValue);
+    }
   }, []);
 
   const getAuth = useCallback((): boolean => {
     const stored = localStorage.getItem(STORAGE_KEY);
-    return stored ? JSON.parse(stored) : false;
+    if (!stored) return false;
+
+    const decryptedValue = decrypt(stored);
+    if (decryptedValue === "true") return true;
+    if (decryptedValue === "false") return false;
+
+    return false;
   }, []);
 
   const removeAuth = useCallback(() => {
